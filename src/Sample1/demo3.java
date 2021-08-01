27457e5465a6537b1c0c3596482b36cc09f0a858
RASHID <RASHID@DESKTOP-M1G4KTS>
2021-08-01 15:24:58 +0530

    [saurabh]: demo3 created

--- src/Sample1/demo3.java
+++ src/Sample1/demo3.java
@@ -3,8 +3,7 @@ package Sample1;
 public class demo3 {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("how are you prashant.....By saurabh");
+		System.out.println("how are you.... by saurabh");
 		
 	}
 
